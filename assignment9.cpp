0ca94c65979fb17ef8d3fd7e24fa1be9c764348d
Saalim398 <saalimkhan1304@gmail.com>
2024-12-09 14:34:07 +0530

    �

--- assignment9.cpp
+++ assignment9.cpp
@@ -5,7 +5,7 @@ int fact(int n) {
     if (n == 0 || n == 1) {
         return 1;
     } else {
-        return n * fact(n - 1); // Correct recursive call
+        return n * fact(n - 1); 
     }
 }
 
